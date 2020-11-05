66f4593554fd2bf7325373cb313cc8199772b71a
benjaquintana <54126753+benjaquintana@users.noreply.github.com>
2020-11-05 08:32:59 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,13 +5,6 @@ $(function(){
       $('.navegacion_principal').slideToggle();
     });
 
-    //Controles Video Radio
-    var video1 = document.getElementById('video_index');
-        Enabler.loadModule(studio.module.ModuleId.VIDEO, function() {
-        studio.video.Reporter.attach('video_index', video1);
-        });
-});
-
 $(function() {
    
     //Página Sobre Nosotros
